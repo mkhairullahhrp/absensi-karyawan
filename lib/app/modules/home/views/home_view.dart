d64e650079619d79f25456fbb5db9d85165d4470
M Khairullah <mkhairullahhrp15@gmail.com>
2023-12-06 19:14:05 +0700

    update today presence

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -119,27 +119,46 @@ class HomeView extends GetView<HomeController> {
                   borderRadius: BorderRadius.circular(20),
                   color: Colors.grey[200],
                 ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Column(
+                child: StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+                  stream: controller.streamTodayPresence(),
+                  builder: (context, snapToday) {
+                    if (snapToday.connectionState == ConnectionState.waiting) {
+                      return Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+                    Map<String, dynamic>? dataToday = snapToday.data?.data();
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
                       children: [
-                        Text("Masuk"),
-                        Text("-")
+                        Column(
+                          children: [
+                            Text("Masuk", 
+                              // style: TextStyle(
+                              //   fontWeight: FontWeight.bold,
+                              // ),
+                            ),
+                            Text(dataToday?["masuk"] == null ? "-" : "${DateFormat.jms().format(DateTime.parse(dataToday!['masuk']['date']))}")
+                          ],
+                        ),
+                        Container(
+                          width: 2,
+                          height: 40,
+                          color: Colors.grey,
+                        ),
+                        Column(
+                          children: [
+                            Text("Keluar", 
+                              //   style: TextStyle(
+                              //   fontWeight: FontWeight.bold,
+                              // ),
+                            ),
+                            Text(dataToday?["keluar"] == null ? "-" : "${DateFormat.jms().format(DateTime.parse(dataToday!['keluar']['date']))}")
+                          ],
+                        ),
                       ],
-                    ),
-                    Container(
-                      width: 2,
-                      height: 40,
-                      color: Colors.grey,
-                    ),
-                    Column(
-                      children: [
-                        Text("Keluar"),
-                        Text("-")
-                      ],
-                    ),
-                  ],
+                    );
+                  }
                 ),
               ),
               SizedBox(height: 10,),
@@ -187,7 +206,7 @@ class HomeView extends GetView<HomeController> {
                     physics: NeverScrollableScrollPhysics(),
                     itemCount: snapPresence.data?.docs.length,
                     itemBuilder: (context, index) {
-                      Map<String, dynamic> data = snapPresence.data!.docs.reversed.toList()[index].data();
+                      Map<String, dynamic> data = snapPresence.data!.docs[index].data();
                       return Padding(
                         padding: const EdgeInsets.only(bottom: 20),
                         child: Material(
